5dd255cd0a1a9006b545fdaffa0ec02840877e82
Boobalamurugan S <118203398+Boobalamurugan@users.noreply.github.com>
2024-09-26 05:28:34 +0000

    day 12 process

---
+++ Day_11-15/day12-2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool ispar(string str){
+    stack<char> st;
+    for(int i=0;i<str.size()-1;i++){
+        if(int i=0;)
+    }
+}
+
+
+int main(){
+    string str="{[][()]}";
+    if(ispar(str)) cout<< "Balanced";
+    else cout<<"not balanced";   
+}
